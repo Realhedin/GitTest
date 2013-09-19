08ac46033e1279024ed665878a5f909e33f1747b
Realhedin <korwork87@yahoo.com>
2013-09-19 13:46:38 +0400

    new project was created

---
+++ src/main/java/TestGitJUnitLoggging/App.java
@@ -0,0 +1,13 @@
+package TestGitJUnitLoggging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
