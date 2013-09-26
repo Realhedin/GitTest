b2f80f6debcbe9bd17165dc0d43488b7f82efcea
Realhedin <korwork87@yahoo.com>
2013-09-26 16:42:51 +0400

    100% coverage for Calculator.class

--- src/test/java/TestGitJUnitLoggging/CalculatorTest.java
+++ src/test/java/TestGitJUnitLoggging/CalculatorTest.java
@@ -3,6 +3,11 @@ package TestGitJUnitLoggging;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +19,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
     private Calculator calc;
+
 
     @Before
     public void init() {
@@ -28,8 +34,87 @@ public class CalculatorTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void exceptionTest() {
+         public void addNullTest() {
+        calc.add(null,null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addExcTest1() {
+        calc.add(null,6);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addExcTest2() {
         calc.add(6,null);
+    }
+
+
+     @Test
+    public void subTest() {
+        Integer actual = calc.substract(8,2);
+        Assert.assertEquals(Integer.valueOf(6),actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void subNullTest() {
+        calc.substract(null,null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void subExcTest1() {
+        calc.substract(null,6);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void subExcTest2() {
+        calc.substract(6,null);
+    }
+
+    @Test
+    public void mulTest() {
+        Integer actual = calc.multiply(4,2);
+        Assert.assertEquals(Integer.valueOf(8),actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void mulNullTest() {
+        calc.multiply(null,null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void mulExcTest1() {
+        calc.multiply(null,6);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void mulExcTest2() {
+        calc.multiply(6,null);
+    }
+
+    @Test
+    public void divTest() {
+        Float actual = calc.division(8,2);
+        Assert.assertEquals(Float.valueOf(4),actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void divNullTest() {
+        calc.division(null,null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void divExcTest1() {
+        calc.division(null,6);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void divExcTest2() {
+        calc.division(6,null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void divEquals0Test() {
+        calc.division(6,0);
     }
 
 }
